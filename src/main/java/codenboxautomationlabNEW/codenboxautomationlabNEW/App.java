84defa9d3b7faec330dd30847173e38d7ecd0b89
AdnanMarah <marahadnan19@gmail.com>
2025-04-21 21:57:17 +0300

    update new

---
+++ src/main/java/codenboxautomationlabNEW/codenboxautomationlabNEW/App.java
@@ -0,0 +1,10 @@
+package codenboxautomationlabNEW.codenboxautomationlabNEW;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
